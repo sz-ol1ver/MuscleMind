54d1c40808f272a00cb3d0864adf88f58cb5177f
oliver <65410637+sz-ol1ver@users.noreply.github.com>
2025-12-18 13:48:42 +0100

    login.html start

--- MuscleMind/backend/server.js
+++ MuscleMind/backend/server.js
@@ -25,7 +25,7 @@ app.use(
 //!Routing
 //?Főoldal:
 router.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, '../frontend/html/index.html'));
+    response.sendFile(path.join(__dirname, '../frontend/html/login.html'));
 });
 
 //!API endpoints
