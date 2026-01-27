12badc8c0ad8a5e24053f4d7f6b7e070331aab8c
oliver <65410637+sz-ol1ver@users.noreply.github.com>
2026-01-27 15:13:47 +0100

    register.js kommentek

--- MuscleMind/frontend/javascript/register.js
+++ MuscleMind/frontend/javascript/register.js
@@ -37,16 +37,17 @@
 */
 document.addEventListener('DOMContentLoaded',()=>{
     //! id
-    //? last-name -- Vezeteknev
-    //? first-name -- Keresztnev
-    //? userName -- felhasznalonev
-    //? email-regist -- email cim
-    //? password-regist -- jelszo
-    //? password-confirm-regist -- jelszo megerosites
-    //? password-feedback -- jelszo egyezes visszajelzo
-    //? regist-in -- regisztracio gomb
+    //? last-name -- Vezeteknev --> lastN
+    //? first-name -- Keresztnev --> fistN
+    //? userName -- felhasznalonev --> userName
+    //? email-regist -- email cim --> email
+    //? password-regist -- jelszo --> pass
+    //? password-confirm-regist -- jelszo megerosites --> passConf
+    //? password-feedback -- jelszo egyezes visszajelzo --> feedBack
+    //? regist-in -- regisztracio gomb --> registBtn
 
-    const lastN = document.getElementById('last-name');
+    //? mezok lekérdezése
+    const lastN = document.getElementById('last-name'); 
     const firstN = document.getElementById('first-name');
     const userName = document.getElementById('userName');
     const email = document.getElementById('email-regist');
@@ -55,26 +56,36 @@ document.addEventListener('DOMContentLoaded',()=>{
     const feedBack = document.getElementById('password-feedback');
     const registBtn = document.getElementById('regist-in');
 
-    const patterUser = /^[a-z0-9]*$/
+    //? Felhasznalonev megengedett karakterei
+    const patterUser = /^[a-z0-9]*$/ 
+    //? Jelszo megengedett karakterei
     const patterPass = /^[a-zA-Z0-9]*$/
     
+    //! jelszo ill. jelszo elotti mezok vizsgalata
+    // folyamatos ellenőrzés minden karakter után a jelszo mezoben
     pass.addEventListener('input', ()=>{
+        //* Vezeteknev - Keresztnev ures-e
         if(lastN.value != '' && firstN.value !=''){
             feedBack.innerHTML='';
             lastN.style.border = '1px solid white';
             firstN.style.border = '1px solid white';
+            //* Felhasznalonev ures-e
             if(userName.value!=''){
                 feedBack.innerHTML ='';
                 userName.style.border = '1px solid white';
+                //* Felhasznalonev karakter vizsgalata
                 if(patterUser.test(userName.value)==true){
                     userName.style.border = '1px solid white';
                     feedBack.innerHTML = '';
+                    //* Email cim ures-e
                     if(email.value!=''){
                         feedBack.innerHTML = '';
                         email.style.border = '1px solid white';
+                        //* Jelszo ures-e
                         if(pass.value!=''){
                             feedBack.innerHTML = '';
                             pass.style.border = '1px solid white';
+                            //* Jelszo karakter vizsgalata
                             if(patterPass.test(pass.value)==true){
                                 feedBack.innerHTML = '';
                                 pass.style.border = '1px solid white';
@@ -127,18 +138,21 @@ document.addEventListener('DOMContentLoaded',()=>{
             passConf.disabled = true;
         }
     })
+
+    //! jelszo megerosito folyamatos vizsgalata
     passConf.addEventListener('input', ()=>{
+        //* Ures-e barmelyik jelszo elotti mezo
         if(lastN.value == '' || firstN.value=='' || userName.value=='' || email.value==''){
             passConf.value = '';
             passConf.disabled = true;
             pass.value = '';
-        }else if(pass.value == passConf.value){
+        }else if(pass.value == passConf.value){//* Egyezik-e a jelszo
             feedBack.style.color = 'lightgreen';
             feedBack.innerHTML = 'A jelszó megegyezik!';
             registBtn.disabled = false;
-        }else if(passConf.value == ''){
+        }else if(passConf.value == ''){//* Ures jelszo megerosito
             feedBack.innerHTML ='';
-        }else{
+        }else{//* Minden mas esetben
             feedBack.style.color = 'red';
             feedBack.innerHTML = 'A két jelszó különbözik!';
         }
