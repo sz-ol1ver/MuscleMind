4636529bfc328ca724b68246f3097d4e056f11fb
oliver <65410637+sz-ol1ver@users.noreply.github.com>
2026-01-31 16:57:02 +0100

    kerdoiv html/js kommentek nelkul

--- MuscleMind/frontend/javascript/question.js
+++ MuscleMind/frontend/javascript/question.js
@@ -22,7 +22,7 @@ const ans = [
 
     [
         "tömegnövelés",
-        "zálkásítás",
+        "szálkásítás",
         "szintentartás"
     ],
 
@@ -69,3 +69,123 @@ const userProfile = {
     dietType: null,
     mealsPerDay: null
 };
+
+//? id
+// #question -- kerdesek helye (h3)
+// #answ -- valaszok helye
+// #submit-qtn -- kovetkezo kerdes
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const question = document.getElementById('question');
+    const answer = document.getElementById('answ');
+    const next = document.getElementById('submit-qtn');
+
+    let currentIndex = 0;
+    let selectedValue = null;
+
+    next.addEventListener('click', ()=>{
+        console.log(currentIndex);
+        if(currentIndex < custom_qtn.length){
+            const rangeValue = document.getElementById("range_input").value;
+            switch (currentIndex){
+                case 0: userProfile.weight = Number(rangeValue); break;
+                case 1: userProfile.age = Number(rangeValue); break;
+                case 2: userProfile.height = Number(rangeValue); break;
+            }
+        }else if(currentIndex >= custom_qtn.length){
+            if(!selectedValue){
+                window.alert('Válassz egy lehetőséget');
+                return;
+            }
+            console.log(selectedValue)
+            const answIndex = currentIndex-3;
+            switch (answIndex){
+                case 0: userProfile.gender = selectedValue; break;
+                case 1: userProfile.goal = selectedValue; break;
+                case 2: userProfile.experienceLevel = selectedValue; break;
+                case 3: userProfile.trainingDays = selectedValue; break;
+                case 4: userProfile.trainingLocation = selectedValue; break;
+                case 5: userProfile.dietType = selectedValue; break;
+                case 6: userProfile.mealsPerDay = selectedValue; break;
+        }}
+        if(currentIndex+1 == 10){
+            console.log(userProfile)
+        }else{
+            currentIndex++;
+            loadQuestion();
+        }
+    })
+
+    function loadQuestion(){
+        answer.innerHTML = '';
+        selectedValue = null;
+
+        if(currentIndex < custom_qtn.length){
+            question.innerHTML = custom_qtn[currentIndex]
+
+            const rangeIn = document.createElement('input');
+            rangeIn.type = 'range';
+            rangeIn.id = 'range_input';
+
+            if(currentIndex == 0){
+                rangeIn.min = 40;
+                rangeIn.max = 200;
+                rangeIn.value = 70;
+            }else if(currentIndex == 1){
+                rangeIn.min = 18;
+                rangeIn.max = 99;
+                rangeIn.value = 25;
+            }else if(currentIndex == 2){
+                rangeIn.min = 140;
+                rangeIn.max = 220;
+                rangeIn.value = 175;
+            }
+
+            const rangeFeedback = document.createElement('span');
+            rangeFeedback.id = 'rangeValue';
+            rangeFeedback.innerHTML = rangeIn.value;
+            rangeFeedback.style.fontWeight = "bold";
+
+            rangeIn.addEventListener('input', ()=>{
+                rangeFeedback.innerHTML = rangeIn.value;
+            })
+
+            answer.appendChild(rangeIn);
+            answer.appendChild(rangeFeedback);
+        }else{
+            const answIndex = currentIndex-3;
+            question.innerHTML = ans_qtn[answIndex];
+
+            const optionsDiv = document.createElement("div");
+            optionsDiv.classList.add(
+                "d-flex",
+                "flex-wrap",
+                "gap-3",
+                "justify-content-center"
+            );
+
+            for(let i = 0; i< ans[answIndex].length; i++){
+                const option = ans[answIndex][i];
+
+                const btn = document.createElement('button');
+                btn.type = 'button';
+                btn.innerHTML = option;
+                btn.classList.add('btn', 'btn-outline-dark');
+
+                btn.addEventListener('click', ()=>{
+                    selectedValue = option;
+
+                    let buttons = document.querySelectorAll('button');
+                    for(let j = 0; j<buttons.length;j++){
+                        buttons[j].classList.remove('active');
+                    }
+                    btn.classList.add('active');
+                })
+                optionsDiv.appendChild(btn)
+            }
+            answer.appendChild(optionsDiv);
+        }
+    }
+
+    loadQuestion();
+})
