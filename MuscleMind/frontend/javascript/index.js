ca0cfac08ee1e78f9c307afa743daa67461e34f1
Dragon <tamasdavid02@gmail.com>
2026-02-23 00:48:29 +0100

    ranglist.html created, loading-overlay linkable for all pages

--- MuscleMind/frontend/javascript/index.js
+++ MuscleMind/frontend/javascript/index.js
@@ -169,25 +169,4 @@ document.addEventListener("DOMContentLoaded", () => {
   //   if(select.value)
   // })
 
-
-  //Loading overlay------------------------------------
-  const loader = document.getElementById("loading-overlay");
-  
-  const allLinks = document.querySelectorAll('a[href]:not([href=""]):not([href^="#"])');
-
-  allLinks.forEach(link => {
-    link.addEventListener("click", (e) => {
-      const targetUrl = link.href;
-
-      if (!targetUrl || targetUrl === window.location.href) return;
-
-      e.preventDefault();
-      
-      loader.classList.add("active");
-
-      setTimeout(() => {
-        window.location.href = targetUrl;
-      }, 700); 
-    });
-  });
 });
