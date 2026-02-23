c2accd070b6369e8ac01ea25755992e0f0cc8a60
Dragon <tamasdavid02@gmail.com>
2026-02-24 00:47:23 +0100

    Profile.html reszponzivitás
    Most már nem törik szét az oldal ha kisebb lesz a kijelző méret hanem egymás alá beugranak a kérdések.

--- MuscleMind/frontend/javascript/profile.js
+++ MuscleMind/frontend/javascript/profile.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const purposeButtons = document.querySelectorAll(".purpose");
+    const purposeButtons = document.querySelectorAll(".purpose")
 
     purposeButtons.forEach(button => {
         button.addEventListener("click", ()=>{
-            const parentBlock = this.closest(".questions");
-            
-            parentBlock.querySelectorAll(".purpose").forEach(btn => {
-                btn.classList.remove("active");
-            });
-
-            this.classList.add("active");
+            const clickedButton = event.currentTarget;
+            const parentBlock = clickedButton.closest(".questions")
+            if(parentBlock){
+                parentBlock.querySelectorAll(".purpose").forEach(btn =>{
+                    btn.classList.remove("active")
+                })
+                clickedButton.classList.add("active")
+            }
         });
     });
 });
