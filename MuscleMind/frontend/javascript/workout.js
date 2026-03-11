68c725a78f1bee7260a29594c98b324df65c2cf4
oliver <65410637+sz-ol1ver@users.noreply.github.com>
2026-03-11 02:19:52 +0100

    new-workout frontend
    workout.js --> disable if not selected
    css stlye

---
+++ MuscleMind/frontend/javascript/workout.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const dayButtons = document.querySelectorAll('.day-box');
+    const active = document.querySelectorAll('.active');
+    const rest = document.getElementById('rest');
+    const select = document.getElementById('gyakorlat');
+    const set = document.getElementById('set');
+    const rep = document.getElementById('rep');
+    const weight = document.getElementById('weight');
+    const add = document.getElementById('add');
+
+    if(active.length != 1){
+        rest.disabled = true;
+        select.disabled = true;
+        set.disabled = true;
+        rep.disabled = true;
+        weight.disabled = true;
+        add.disabled = true;
+    }
+
+    for (let i = 0; i < dayButtons.length; i++) {
+    dayButtons[i].addEventListener('click', function() {
+
+        for (let j = 0; j < dayButtons.length; j++) {
+            dayButtons[j].classList.remove('active');
+        }
+        this.classList.add('active');
+        rest.disabled = false;
+        select.disabled = false;
+        set.disabled = false;
+        rep.disabled = false;
+        weight.disabled = false;
+        add.disabled = false;
+    });
+
+    rest.addEventListener('change', ()=>{
+        if(rest.checked == true){
+            select.disabled = true;
+            set.disabled = true;
+            rep.disabled = true;
+            weight.disabled = true;
+        }else{
+            select.disabled = false;
+            set.disabled = false;
+            rep.disabled = false;
+            weight.disabled = false;
+        }
+    })
+    
+}
+})
