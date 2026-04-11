b2e576ed4725fefe901140d0d4fa1acce2445ed1
oliver <65410637+sz-ol1ver@users.noreply.github.com>
2026-04-12 00:09:54 +0200

    global.js
    + id (navbar)
    + GET /is-admin
    + if admin add btn (global.js)

--- MuscleMind/frontend/javascript/global.js
+++ MuscleMind/frontend/javascript/global.js
@@ -1,6 +1,10 @@
-import { postLogout } from "./api.js";
+import { postLogout, getFetch } from "./api.js";
 
 document.addEventListener('DOMContentLoaded', ()=>{
+    //!admin btn
+    checkAdmin();
+
+    //!logout
     const logoutBtn = document.getElementById('logout');
     logoutBtn.addEventListener('click', ()=>{
         logout();
@@ -13,5 +17,43 @@ async function logout() {
         window.location.href = '/bejelentkezes';
     } catch (error) {
         console.error(error.message)
+    }
+}
+async function checkAdmin() {
+    //? side-nav-list
+    //? mobil-nav-list
+    try {
+        const data = await getFetch('http://127.0.0.1:3000/api/auth/is-admin');
+        const sn = document.getElementById('side-nav-list');
+        const mn = document.getElementById('mobil-nav-list');
+        if(data.admin == 1){
+            // side navconst 
+            const li1 = document.createElement('li');
+            li1.className = 'nav-item';
+            li1.style.border = '1px solid white'
+
+            const a1 = document.createElement('a');
+            a1.className = 'nav-link';
+            a1.href = '/admin';
+            a1.innerHTML = 'Admin';
+
+            li1.appendChild(a1);
+            sn.appendChild(li1);
+
+            // mobil navconst 
+            const li2 = document.createElement('li');
+            li2.className = 'nav-item';
+            li2.style.border = '1px solid white'
+
+            const a2 = document.createElement('a');
+            a2.className = 'nav-link';
+            a2.href = '/admin';
+            a2.innerHTML = 'Admin';
+
+            li2.appendChild(a2);
+            mn.appendChild(li2);
+        }
+    } catch (error) {
+        console.error(error.message);
     }
 }
