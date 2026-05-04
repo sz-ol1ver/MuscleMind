489e9aa32391dbf786fd56f989fe08e65b851f2d
oliver <65410637+sz-ol1ver@users.noreply.github.com>
2026-05-04 20:07:34 +0200

    admin bug fix

--- MuscleMind/backend/middleware/isAdmin.middleware.js
+++ MuscleMind/backend/middleware/isAdmin.middleware.js
@@ -4,8 +4,8 @@ const path = require('path');
 async function requireAdmin(req, res, next) {
     try {
         const currentAdminStatus = await db.ifAdmin(req.session.user.id);
-        req.session?.user?.admin = currentAdminStatus;
-        const isAdmin = req.session?.user?.admin;
+        req.session.user.admin = currentAdminStatus;
+        const isAdmin = req.session.user.admin;
         if (isAdmin != 1) {
             return res.sendFile(path.join(__dirname, '../views/403.html'))
         }
